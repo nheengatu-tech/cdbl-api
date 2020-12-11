896f0e3c796ecbd2680170e18481ca0682fbce33
Roberto <robertoccamposn@gmail.com>
2020-12-11 11:17:31 -0400

    fix: remove cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var authRoutes = require('./controllers/authController')
 
 var app = express()
 app.use(json())
-app.use(cors({ origin:'http://localhost:3000' }))
+// app.use(cors({ origin:'http://localhost:3000' }))
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
